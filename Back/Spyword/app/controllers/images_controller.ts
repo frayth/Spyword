5c31f625f713a349c4b87f69e3ee0020bcecbbf3
frayth <ceresolilauris@gmail.com>
2025-05-02 19:12:56 +0200

    t

--- Back/Spyword/app/controllers/images_controller.ts
+++ Back/Spyword/app/controllers/images_controller.ts
@@ -1,11 +1,15 @@
 import type { HttpContext } from '@adonisjs/core/http'
 import path from 'node:path'
 import fs from 'node:fs'
+
 export default class ImagesController {
   async show({ response, params }: HttpContext) {
+    const baseURl =
+      process.env.node_env === 'development' ? `${process.cwd()}` : `${process.cwd()}/..`
+    console.log(baseURl)
     const imageName = params.name
     const folderName = params.folder
-    const imagePath = `${process.cwd()}/public/images/${folderName}/${imageName}`
+    const imagePath = `${baseURl}/public/images/${folderName}/${imageName}`
     console.log(imagePath)
     if (fs.existsSync(imagePath)) {
       response.download(path.resolve(imagePath))
