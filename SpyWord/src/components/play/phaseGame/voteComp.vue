65e9706959fddeb88bbeb70e45ebf3b0ce0203f3
frayth <ceresolilauris@gmail.com>
2025-06-25 00:06:01 +0200

    ajout visualisation des joueurs qui n'ont pas voté

--- SpyWord/src/components/play/phaseGame/voteComp.vue
+++ SpyWord/src/components/play/phaseGame/voteComp.vue
@@ -2,12 +2,12 @@
   <div class="w-full h-full  grid  ">
     <!--affiche liste si joueur n'a pas vote-->
     <listForVote v-if="!playerHasVoted && playerIsAlive" class="w-full  "></listForVote>
-    
+
     <!--affiche ecran d'attente si joueur a vote-->
     <div v-else-if="playerHasVoted && playerIsAlive" class="bg-gray-900 h-full w-full flex flex-col items-center justify-center">
       <div
       class=" flex flex-col items-center justify-center   text-white p-10 animate-fade-in border b-amber rounded-2xl shadow-[0_0_20px_rgba(255,255,255,0.3)] lg:min-w-[400px] md:min-w-[400px]"
-      
+
     >
       <!-- Avatar -->
       <div
@@ -24,11 +24,28 @@
         class="w-12 h-12 border-4 border-transparent border-t-blue-500 rounded-full animate-spin mt-6"
       ></div>
       <p class="text-lg font-semibold mt-3 text-gray-300 text-shadow">
-        En attente des votes...
+        En attente des autres joueurs...
       </p>
+<div class="flex gap-4 justify-center items-center flex-wrap mt-3">
+  <div
+    v-for="player in currentGame.users.filter(user => !user.gameStat?.asVoted && user.gameStat?.isAlive)"
+    :key="player.id"
+    class="flex flex-col items-center space-y-1"
+  >
+    <div class="w-50px h-50px rounded-full overflow-hidden border-2 border-gray-300 shadow-sm">
+      <portraitComp :url="player.gameStat?.urlAvatar!" />
+    </div>
+    <div
+      class="max-w-80px text-xs text-gray-700 font-medium bg-white px-1 py-0.5 rounded-md shadow-sm truncate overflow-hidden whitespace-nowrap text-center"
+      :title="player.fullName"
+    >
+      {{ player.fullName }}
+    </div>
+  </div>
+</div>
     </div>
     </div>
-    
+
     <div v-else class="flex flex-col items-center justify-center h-full w-full bg-gray-900 text-white p-10 animate-fade-in">
       <p class="text-xl font-bold mt-4 text-center text-white text-shadow">
         Vous êtes mort
