2e49c0797a011fd477695b4834b93791ec498c2c
frayth <ceresolilauris@gmail.com>
2025-07-04 18:37:35 +0200

    fdsfds

--- Back/Spyword/config/database.ts
+++ Back/Spyword/config/database.ts
@@ -1,11 +1,20 @@
 import app from '@adonisjs/core/services/app'
+import { fileURLToPath } from 'node:url'
 import path from 'node:path'
 import { defineConfig } from '@adonisjs/lucid'
+
+const Filename = fileURLToPath(import.meta.url)
+const Dirname = path.dirname(Filename)
+
+function resolveProdDbPath() {
+  const basePath = Dirname.includes('/build')
+    ? path.join(Dirname, '../production/db/prod.sqlite3')
+    : path.join(process.cwd(), 'production/db/prod.sqlite3')
+
+  return basePath
+}
 const isProd = process.env.NODE_ENV === 'production'
-console.log(
-  '> DB PATH:',
-  isProd ? path.join(process.cwd(), 'production/db/prod.sqlite3') : app.tmpPath('dev.sqlite3')
-)
+console.log('> DB PATH:', isProd ? resolveProdDbPath() : app.tmpPath('dev.sqlite3'))
 const dbConfig = defineConfig({
   connection: 'sqlite',
   connections: {
