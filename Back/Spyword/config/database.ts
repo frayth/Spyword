90432e1dfddf75c7bf5522d3cb3dd2b4e893a2e3
frayth <ceresolilauris@gmail.com>
2025-07-04 18:20:58 +0200

    dd

--- Back/Spyword/config/database.ts
+++ Back/Spyword/config/database.ts
@@ -3,7 +3,7 @@ import { defineConfig } from '@adonisjs/lucid'
 const isProd = process.env.NODE_ENV === 'production'
 console.log(
   '> DB PATH:',
-  isProd ? app.makePath('production/db/prod.sqlite3') : app.tmpPath('dev.sqlite3')
+  isProd ? app.makePath('../production/db/prod.sqlite3') : app.tmpPath('dev.sqlite3')
 )
 const dbConfig = defineConfig({
   connection: 'sqlite',
@@ -11,7 +11,9 @@ const dbConfig = defineConfig({
     sqlite: {
       client: 'better-sqlite3',
       connection: {
-        filename: isProd ? app.makePath('production/db/prod.sqlite3') : app.tmpPath('dev.sqlite3'),
+        filename: isProd
+          ? app.makePath('../production/db/prod.sqlite3')
+          : app.tmpPath('dev.sqlite3'),
       },
       useNullAsDefault: true,
       migrations: {
